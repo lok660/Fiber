b4641fd0ee05e092cd7ae544feff90dfac8958a9
lok660 <y19971@126.com>
2020-09-04 22:49:43 +0800

    feat:Component构造

---
+++ src/didact/Component.js
@@ -0,0 +1,15 @@
+import { updateInstance } from './render'
+
+export class Component {
+  constructor(props) {
+    this.props = props
+    this.state = this.state || {}
+  }
+  setState (partialState) {
+    this.state = { ...this.state, partialState }
+    updateInstance(this.__internalInstance)
+  }
+  render () {
+    return null
+  }
+}
